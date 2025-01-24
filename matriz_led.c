37046c4749a31eb97bd835ab66b364558a864ce7
Guilherme <202311190034@ifba.edu.br>
2025-01-24 17:54:42 -0300

    Correção da ativação dos LEDs

--- matriz_led.c
+++ matriz_led.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "hardware/adc.h"
 #include "pico/bootrom.h"
 #include "matriz_led.pio.h"
 
@@ -17,9 +15,9 @@ void bootsel(){
 }
 
 //TECLADO
-// definição das colunas e das linhas e mapeamento do teclado
-uint8_t coluna[4] = {18,19,20,21};
-uint8_t linha[4] = {22,26,27,28};
+//definição das colunas e das linhas e mapeamento do teclado
+uint8_t coluna[4] = {21,20,19,18};
+uint8_t linha[4] = {28,27,26,22};
 
 char teclas[4][4] = {
     '1', '2', '3', 'A',
@@ -70,8 +68,7 @@ uint matrix_rgb(float r, float g, float b)
 void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
 
     for (int16_t i = 0; i < NUM_PIXELS; i++) {
-
-            valor_led = matrix_rgb(r, g, b);
+            valor_led = matrix_rgb(desenho[i] * r, desenho[i] * g, desenho[i] * b);
             pio_sm_put_blocking(pio, sm, valor_led);
         }
     }
@@ -82,11 +79,11 @@ double apagar_leds[25] = {0.0, 0.0, 0.0, 0.0, 0.0,           //Apagar LEDs da ma
                           0.0, 0.0, 0.0, 0.0, 0.0,
                           0.0, 0.0, 0.0, 0.0, 0.0};
 
-double desenho1_1[25] = {0.3, 0.0, 0.0, 0.0, 0.3,           //Desenho Tecla 1 Parte 1
-                         0.0, 0.3, 0.0, 0.3, 0.0, 
+double desenho1_1[25] = {0.0, 0.0, 0.3, 0.0, 0.0,           //Desenho Tecla 1 Parte 1
+                         0.0, 0.0, 0.3, 0.0, 0.0, 
                          0.0, 0.0, 0.3, 0.0, 0.0,
-                         0.0, 0.0, 0.3, 0.0, 0.0,
-                         0.0, 0.0, 0.3, 0.0, 0.0};
+                         0.0, 0.3, 0.0, 0.3, 0.0,
+                         0.3, 0.0, 0.0, 0.0, 0.3};
 
 double desenho1_2[25] = {0.3, 0.3, 0.3, 0.3, 0.3,           //Desenho Tecla 1 Parte 2
                          0.3, 0.0, 0.0, 0.0, 0.3, 
@@ -94,17 +91,17 @@ double desenho1_2[25] = {0.3, 0.3, 0.3, 0.3, 0.3,           //Desenho Tecla 1 Pa
                          0.3, 0.0, 0.0, 0.0, 0.3,
                          0.3, 0.3, 0.3, 0.3, 0.3};
 
-double desenho1_3[25] = {0.3, 0.0, 0.0, 0.0, 0.3,           //Desenho Tecla 1 Parte 3
+double desenho1_3[25] = {0.3, 0.3, 0.3, 0.3, 0.3,           //Desenho Tecla 1 Parte 3
                          0.3, 0.0, 0.0, 0.0, 0.3, 
                          0.3, 0.0, 0.0, 0.0, 0.3,
                          0.3, 0.0, 0.0, 0.0, 0.3,
-                         0.3, 0.3, 0.3, 0.3, 0.3};
+                         0.3, 0.0, 0.0, 0.0, 0.3};
 
-double desenho1_4[25] = {0.3, 0.0, 0.0, 0.0, 0.3,           //Desenho Tecla 1 Parte 4
-                         0.3, 0.0, 0.3, 0.0, 0.3, 
+double desenho1_4[25] = {0.0, 0.3, 0.3, 0.3, 0.0,           //Desenho Tecla 1 Parte 4
+                         0.3, 0.3, 0.3, 0.3, 0.3, 
                          0.3, 0.0, 0.3, 0.0, 0.3,
                          0.3, 0.0, 0.3, 0.0, 0.3,
-                         0.0, 0.3, 0.3, 0.3, 0.0};
+                         0.3, 0.0, 0.0, 0.0, 0.3};
 
 double desenho1_5[25] = {0.0, 0.0, 0.3, 0.0, 0.0,           //Desenho Tecla 1 Parte 5
                          0.0, 0.0, 0.3, 0.0, 0.0, 
@@ -113,9 +110,9 @@ double desenho1_5[25] = {0.0, 0.0, 0.3, 0.0, 0.0,           //Desenho Tecla 1 Pa
                          0.0, 0.0, 0.3, 0.0, 0.0};
 
 double desenho1_6[25] = {0.3, 0.0, 0.0, 0.0, 0.3,           //Desenho Tecla 1 Parte 6
-                         0.3, 0.3, 0.0, 0.0, 0.3, 
+                         0.3, 0.0, 0.0, 0.3, 0.3, 
                          0.3, 0.0, 0.3, 0.0, 0.3,
-                         0.3, 0.0, 0.0, 0.3, 0.3,
+                         0.3, 0.3, 0.0, 0.0, 0.3,
                          0.3, 0.0, 0.0, 0.0, 0.3};
 
 
@@ -152,76 +149,78 @@ int main()
 
     switch (tecla)
     {
-    case 1:                             // Verifica se a tecla 1 foi pressionada
+    case '1':                             // Verifica se a tecla 1 foi pressionada
 
         desenho_pio(desenho1_1, valor_led, pio, sm, r, g, b);
-        sleep_ms(100);
+        sleep_ms(500);
         desenho_pio(desenho1_2, valor_led, pio, sm, r, g, b);
-        sleep_ms(100);
+        sleep_ms(500);
         desenho_pio(desenho1_3, valor_led, pio, sm, r, g, b);
-        sleep_ms(100);
+        sleep_ms(500);
         desenho_pio(apagar_leds, valor_led, pio, sm, r, g, b);
-        sleep_ms(100);
+        sleep_ms(500);
         desenho_pio(desenho1_4, valor_led, pio, sm, r, g, b);
-        sleep_ms(100);
+        sleep_ms(500);
         desenho_pio(desenho1_5, valor_led, pio, sm, r, g, b);
-        sleep_ms(100);
+        sleep_ms(500);
         desenho_pio(desenho1_6, valor_led, pio, sm, r, g, b);
-        sleep_ms(100);
+        sleep_ms(500);
         desenho_pio(apagar_leds, valor_led, pio, sm, r, g, b);
 
         break;
     
-    case 2:                                 // Verifica se a tecla 2 foi pressionada
+    case '2':                                 // Verifica se a tecla 2 foi pressionada
 
 
 
         break;
 
-    case 3:                                 // Verifica se a tecla 3 foi pressionada
+    case '3':                                 // Verifica se a tecla 3 foi pressionada
 
 
 
         break;
 
-    case 4:                                 // Verifica se a tecla 4 foi pressionada
+    case '4':                                 // Verifica se a tecla 4 foi pressionada
 
 
 
         break;
 
-    case 5:                                 // Verifica se a tecla 5 foi pressionada
-
-
-
-        break;
-    
-    case 6:                                 // Verifica se a tecla 6 foi pressionada
-
-
-
-        break;
-
-    case 7:                                 // Verifica se a tecla 7 foi pressionada
+    case '5':                                 // Verifica se a tecla 5 foi pressionada
 
 
 
         break;
     
-    case 8:                                 // Verifica se a tecla 8 foi pressionada
+    case '6':                                 // Verifica se a tecla 6 foi pressionada
 
 
 
         break;
 
-    case 9:                                 // Verifica se a tecla 9 foi pressionada
+    case '7':                                 // Verifica se a tecla 7 foi pressionada
+
+
+
+        break;
+    
+    case '8':                                 // Verifica se a tecla 8 foi pressionada
+
+
+
+        break;
+
+    case '9':                                 // Verifica se a tecla 9 foi pressionada
 
 
 
         break;
 
     case 'A':                               // Verifica se a tecla A foi pressionada
-         
+        r = 0;
+        g = 0;
+        b = 0;
         apagar_leds;
         break;
 
